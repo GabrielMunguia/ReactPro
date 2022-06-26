5b18a1e89eef82429126314459606f8c208491eb
GabrielMunguia <juayua2011@gmail.com>
2022-06-26 12:48:24 -0600

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div className="bg-slate-800 min-h-screen text-white">
+      <h1 className="text-3xl font-bold underline ">olaaa</h1>
+    </div>
+  );
+};
+
+export default App;
